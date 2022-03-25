55c80250dc0b11b29d30272b4be4a2d22a182ec1
ryPattillo <ryan_pattillo@knights.ucf.edu>
2022-03-24 23:48:40 -0400

    Initial data reading and traiing loop setup

--- read_data.py
+++ read_data.py
@@ -1,6 +1,70 @@
-file = open("Data.txt","r")
-print(file.readline())
+'''
+NOTE: here is a list of all the keys in the dataset
+keys = [
+    "quarter"
+    "stock",
+    "date",
+    "open",
+    "high",
+    "low",
+    "close",
+    "volume",
+    "percent_change_price",
+    "percent_change_volume_over_last_wk",
+    "previous_weeks_volume",
+    "next_weeks_open",
+    "percent_change_next_weeks_price",
+    "days_to_next_dividend",
+    "percent_return_next_dividend"
+]
+'''
+
+def get_data():
+    '''
+    Get dataset of x,y values
+    
+    '''
+
+    # open data file
+    try:
+        stock_data = open("stock_data.txt","r")
+    except:
+        print("Error opening file")
+
+    # keep a list of the stock data
+
+    # the first row contains all the attribute names
+    first_row = True
+
+    y_values = []
+    x_values = []
+
+    for entry in stock_data:
+        if first_row:
+            attributes = entry.split(',')
+            first_row = False
+            continue
+    
+        values = entry.split(',')
+        x_values.append(values)
+        y_values.append(values.pop(14))
+
+    return x_values,y_values,attributes
 
 
-for line in file:
-    print(type(line))
+def dataset_split(data,train_split,test_split,val_split):
+    '''
+    Split the datast into train,test, and val
+    '''
+
+    train_idx = int(train_split * len(data))
+    test_idx = int(test_split * len(data))
+    val_idx = int(val_split * len(data))
+
+    train_data = data[0:train_idx]
+    test_data = data[len(train_data) + 1: len(train_data) + test_idx + 1]
+    val_data = data[len(test_data) + 1: len(test_data) + val_idx + 1]
+
+    assert (len(train_data) + len(test_data) + len(val_data)) == (len(data) - 1)
+
+    return train_data,test_data,val_data
---
+++ train.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+from read_data import get_data, dataset_split
+
+if __name__ == "__main__":
+
+    # Get x and y values
+    x_values,y_values,attributes = get_data()
+
+    # Different dataset splits
+    train_split = 0.70
+    test_split = 0.15
+    val_split = 0.15
+
+    # Split into 
+    x_train, x_test ,x_val = dataset_split(x_values,train_split,test_split,val_split)
+    y_train, y_test ,y_val = dataset_split(y_values,train_split,test_split,val_split)
+
+    '''
+    NOTE: Here is example neural network code
+    TODO: Figure out how to encode the data and the network structure
+
+    model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(15, 1)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(1, activation='softmax')
+    ])
+
+    model.compile(optimizer='adam',
+                loss='sparse_categorical_crossentropy',
+                metrics=['accuracy'])
+
+    model.fit(x_train, y_train, epochs=5)
+    model.evaluate(x_test, y_test)
+    '''
