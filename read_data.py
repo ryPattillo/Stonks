921d54018a2be7b536fbc0c3b82e5c08db65a54d
ryPattillo <ryan_pattillo@knights.ucf.edu>
2022-03-30 18:46:44 -0400

    Set up better data reading and formatting

--- read_data.py
+++ read_data.py
@@ -1,77 +1,30 @@
-import finnhub
-import time
-from sklearn.model_selection import train_test_split
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
 import numpy as np
 
-'''
-NOTE: here is a list of all the keys in the dataset
-keys = [
-    0 - "quarter", [x]
-    1 - "stock", [x]
-    2 - "date",
-    3 - "open",
-    4 - "high",
-    5 - "low",
-    6 - "close",
-    7 - "volume",
-    8 - "percent_change_price",
-    9 - "percent_change_volume_over_last_wk"
-    10 - "previous_weeks_volume",
-    11 - "next_weeks_open",
-    12 - "percent_change_next_weeks_price  [pcw] [y]
-    13 - "days_to_next_dividend",
-    14 - "percent_return_next_dividend"
-]
-'''
+def extract_data():
+    df = pd.read_csv('data.csv')
 
+    # get matrices of the columns
+    values = df.to_numpy()
 
-def get_data():
+    # extract v_values
+    y_values = values[:,12]
 
-    numeric_idx = [0,3,4,5,6,7,8,13,14]
-    finnhub_client = finnhub.Client(api_key="c004igf48v6v49v27a2g")
+    # delete the y values
+    # NOTE: just deleting these for now to test
+    x_values = np.delete(values, 12, 1) 
+    x_values = np.delete(x_values, 2, 1) 
+    x_values = np.delete(x_values, 1, 1) 
+    x_values = np.delete(x_values, 13, 1) 
 
-    #print(finnhub_client.company_profile2(symbol='AAPL'))
+    # convert to float
+    x_values = x_values.astype(np.float32)
+    y_values = y_values.astype(np.float32)
 
-    '''
-    Get dataset of x,y values
-    '''
-    # open data file
-    try:
-        stock_data = open("stock_data.txt","r")
-    except:
-        print("Error opening file")
+    # 1 if > 102 otherwise 0
+    y_values = y_values * (y_values > 102)
+    y_values[y_values > 0 ] = 1
 
-    # the first row contains all the attribute names
-    first_row = True
-
-    y_values = []
-    x_values = []
-
-    calls = 0
-
-    for entry in stock_data:
-        if first_row:
-            attributes = entry.split(',')
-            first_row = False
-            continue
-    
-        # if calls == 59:
-        #     time.sleep(60)
-        #     calls = 0
-
-
-        values = entry.split(',')
-        x_values.append(np.array([float(values[i]) for i in numeric_idx]))
-        y_values.append(1 if float(values.pop(12)) > 20 else 0)
-
-
-        # # profile = finnhub_client.company_profile2(symbol=values[1])
-        # if profile is not None:
-        #     industry = profile['finnhubIndustry']
-        #     print(industry)
-        # calls += 1
-
-        
-    return np.array(x_values),np.array(y_values),attributes
-
+    return x_values,y_values
 
--- train.py
+++ train.py
@@ -1,29 +1,25 @@
 import tensorflow as tf
-from read_data import get_data
+from read_data import extract_data
 from sklearn.model_selection import train_test_split
 import numpy as np
 
 if __name__ == "__main__":
-
     # Get x and y values
-    x_values,y_values,attributes = get_data()
+    x_values,y_values = extract_data()
 
     # Split into train and test
     x_train,x_test,y_train,y_test = train_test_split(x_values,y_values,test_size = 0.20)
 
-    
     #NOTE: Here is example neural network code
     #TODO: Figure out how to encode the data and the network structure
-
     model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(9,1)),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(1)
+    tf.keras.layers.Flatten(input_shape=(13,1)),
+    tf.keras.layers.Dense(60, activation='relu'),
+    tf.keras.layers.Dense(1,activation ='sigmoid')
     ])
-
     model.compile(optimizer='adam',
-                loss='mse',
+                loss='binary_crossentropy',
                 metrics=['accuracy'])
 
-    model.fit(x_train, y_train, epochs=50)
+    model.fit(x_train, y_train, epochs=100)
     model.evaluate(x_test, y_test)
