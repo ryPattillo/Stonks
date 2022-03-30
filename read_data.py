d5d80ffaf1b385ee38f1efff6c60379cb852d949
ryPattillo <ryan_pattillo@knights.ucf.edu>
2022-03-30 16:58:37 -0400

    Traininggit add train.py

--- read_data.py
+++ read_data.py
@@ -1,11 +1,13 @@
 import finnhub
 import time
+from sklearn.model_selection import train_test_split
+import numpy as np
 
 '''
 NOTE: here is a list of all the keys in the dataset
 keys = [
-    0 - "quarter",
-    1 - "stock",
+    0 - "quarter", [x]
+    1 - "stock", [x]
     2 - "date",
     3 - "open",
     4 - "high",
@@ -13,10 +15,10 @@ keys = [
     6 - "close",
     7 - "volume",
     8 - "percent_change_price",
-    9 - "percent_change_volume_over_last_wk", [pcw]
+    9 - "percent_change_volume_over_last_wk"
     10 - "previous_weeks_volume",
     11 - "next_weeks_open",
-    12 - "percent_change_next_weeks_price",
+    12 - "percent_change_next_weeks_price  [pcw] [y]
     13 - "days_to_next_dividend",
     14 - "percent_return_next_dividend"
 ]
@@ -24,6 +26,8 @@ keys = [
 
 
 def get_data():
+
+    numeric_idx = [0,3,4,5,6,7,8,13,14]
     finnhub_client = finnhub.Client(api_key="c004igf48v6v49v27a2g")
 
     #print(finnhub_client.company_profile2(symbol='AAPL'))
@@ -51,38 +55,23 @@ def get_data():
             first_row = False
             continue
     
-        if calls == 59:
-            time.sleep(60)
-            calls = 0
+        # if calls == 59:
+        #     time.sleep(60)
+        #     calls = 0
 
 
         values = entry.split(',')
-        profile = finnhub_client.company_profile2(symbol=values[1])
-        if profile is not None:
-            industry = profile['finnhubIndustry']
-            print(industry)
-        calls += 1
+        x_values.append(np.array([float(values[i]) for i in numeric_idx]))
+        y_values.append(float(values.pop(12)))
+
+
+        # # profile = finnhub_client.company_profile2(symbol=values[1])
+        # if profile is not None:
+        #     industry = profile['finnhubIndustry']
+        #     print(industry)
+        # calls += 1
 
         
-        x_values.append(values)
-        y_values.append(values.pop(14))
-
-    return x_values,y_values,attributes
+    return np.array(x_values),np.array(y_values)/200,attributes
 
 
-def dataset_split(data,train_split,test_split,val_split):
-    '''
-    Split the datast into train,test, and val
-    '''
-
-    train_idx = int(train_split * len(data))
-    test_idx = int(test_split * len(data))
-    val_idx = int(val_split * len(data))
-
-    train_data = data[0:train_idx]
-    test_data = data[len(train_data) + 1: len(train_data) + test_idx + 1]
-    val_data = data[len(test_data) + 1: len(test_data) + val_idx + 1]
-
-    assert (len(train_data) + len(test_data) + len(val_data)) == (len(data) - 1)
-
-    return train_data,test_data,val_data
--- train.py
+++ train.py
@@ -1,29 +1,25 @@
 import tensorflow as tf
-from read_data import get_data, dataset_split
+from read_data import get_data
+from sklearn.model_selection import train_test_split
+import numpy as np
 
 if __name__ == "__main__":
 
     # Get x and y values
     x_values,y_values,attributes = get_data()
 
-    # Different dataset splits
-    train_split = 0.70
-    test_split = 0.15
-    val_split = 0.15
-
-    # Split into 
-    x_train, x_test ,x_val = dataset_split(x_values,train_split,test_split,val_split)
-    y_train, y_test ,y_val = dataset_split(y_values,train_split,test_split,val_split)
+    # Split into train and test
+    x_train,x_test,y_train,y_test = train_test_split(x_values,y_values,test_size = 0.20)
 
     
     #NOTE: Here is example neural network code
     #TODO: Figure out how to encode the data and the network structure
 
     model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(15, 1)),
+    tf.keras.layers.Flatten(input_shape=(9,1)),
     tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(1, activation='softmax')
+    tf.keras.layers.Dense(1)
     ])
 
     model.compile(optimizer='adam',
